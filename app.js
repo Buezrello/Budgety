e4c02e9d5a50a7040df8bcd798e6db69fc4e8804
igorg <igorg@perfectomobile.com>
2017-06-15 12:29:44 +0300

    Clearing Our Input Fields

--- app.js
+++ app.js
@@ -104,7 +104,20 @@ var UIController = (function () {
             // Insert the HTML to the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
             
+        },
+        
+        clearFields: function () {
+            var fields, fieldsArray;
             
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+            
+            fieldsArray = Array.prototype.slice.call(fields);
+            
+            fieldsArray.forEach(function(current, index, array) {
+                current.value = "";
+            });
+            
+            fieldsArray[0].focus();
         },
         
         getDOMstrings: function () {
@@ -148,9 +161,12 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 3. Add iem to UI
         UICtrl.addListItem(newItem, input.type);
         
-        // 4. Calculate the budget
+        // 4. Clear the fields
+        UICtrl.clearFields();
         
-        // 5. Display the budget on UI
+        // 5. Calculate the budget
+        
+        // 6. Display the budget on UI
         
     };
     
