4e15e9f92879d0b5bb8686b821872ef8b636fdca
igorg <igorg@perfectomobile.com>
2017-06-15 13:02:48 +0300

    Updating the Budget: Controller

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ var UIController = (function () {
             return {
                 type: document.querySelector(DOMstrings.inputType).value,  //  will be either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
         
@@ -148,6 +148,17 @@ var controller = (function (budgetCtrl, UICtrl) {
         });
     };
     
+    var updateBudget = function () {
+        
+        // 1. Calculate the budget
+        
+        
+        // 2. Return the budget
+        
+        
+        // 3. Display the budget on UI
+        
+    }; 
     
     var ctrlAddItem = function () {
         var input, newItem;
@@ -155,18 +166,19 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 1. Get field input
         input = UICtrl.getinput();
         
-        // 2. Add item to budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            // 2. Add item to budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         
-        // 3. Add iem to UI
-        UICtrl.addListItem(newItem, input.type);
+            // 3. Add iem to UI
+            UICtrl.addListItem(newItem, input.type);
         
-        // 4. Clear the fields
-        UICtrl.clearFields();
+            // 4. Clear the fields
+            UICtrl.clearFields();
         
-        // 5. Calculate the budget
-        
-        // 6. Display the budget on UI
+            // 5. Calculate and update budget
+            updateBudget();
+        }
         
     };
     
