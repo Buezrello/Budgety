87fa643dc8b864c1355fc9ec849f2f9900587cd3
igorg <igorg@perfectomobile.com>
2017-06-16 14:40:10 +0300

    Updating the Budget: Budget Controller

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ var budgetController = (function () {
         this.value = value;
     };
     
+    var calculateTotal = function (type) {
+        var sum = 0;
+        data.allItems[type].forEach(function (cur) {
+            sum = sum + cur.value;
+        });
+        data.totals[type] = sum;
+    };
+    
     var data = {
         allItems: {
             exp: [],
@@ -22,7 +30,9 @@ var budgetController = (function () {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
     
     return {
@@ -49,6 +59,33 @@ var budgetController = (function () {
             
             // Return the new element
             return newItem;
+        },
+        
+        calculateBudget: function () {
+            
+            // Calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+            
+            // Calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+            
+            // Callate the percentage of income that we spent
+            if (data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+            
+        },
+        
+        getBudget: function () {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
         },
         
         testing: function() {
@@ -151,12 +188,13 @@ var controller = (function (budgetCtrl, UICtrl) {
     var updateBudget = function () {
         
         // 1. Calculate the budget
-        
+        budgetCtrl.calculateBudget();
         
         // 2. Return the budget
-        
+        var budget = budgetCtrl.getBudget();
         
         // 3. Display the budget on UI
+        console.log(budget);
         
     }; 
     
