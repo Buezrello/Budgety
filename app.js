b5acb19ee6e8f2d575bbc28fca890667dfa7e031
igorg <igorg@perfectomobile.com>
2017-06-23 19:01:35 +0300

    Updating the Percentages: UI Controller

--- app.js
+++ app.js
@@ -151,7 +151,8 @@ var UIController = (function () {
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
     }
     
     return {
@@ -219,6 +220,24 @@ var UIController = (function () {
             }
         },
         
+        displayPercentages: function (percentages) {
+            var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+            
+            var nodeListForEach = function (list, callback) {
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            };
+            
+            nodeListForEach(fields, function(current, index) {
+                if (percentages[index] > 0) {
+                    current.textContent = percentages[index] + '%';
+                } else {
+                    current.textContent = '---';
+                }
+            });
+        },
+        
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -273,7 +292,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         var percentage = budgetCtrl.getPercentages();
         
         // 3. Update the UI with the new percentages
-        console.log(percentage);
+        UICtrl.displayPercentages(percentage);
     };
     
     var ctrlAddItem = function () {
